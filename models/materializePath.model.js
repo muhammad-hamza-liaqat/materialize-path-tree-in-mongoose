30c819ba52210983d2085e3039e9b165f9ec53bb
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-02-26 19:04:58 +0500

    newNodeAdded controller code

--- models/materializePath.model.js
+++ models/materializePath.model.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const treeNodeSchema = new mongoose.Schema({
-  name: String,
-  path: String
+const materializePathSchema = new Schema({
+    _id: {type: String, required: true},
+    path: {type: String, default: null}
 });
 
-const MaterializePathModel = mongoose.model('MaterializePath', treeNodeSchema);
+const MaterializePathModel = mongoose.model('MaterializePath', materializePathSchema);
 
 module.exports = MaterializePathModel;
